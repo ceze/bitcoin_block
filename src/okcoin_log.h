d0a1d3d849ae6c80f571e3eb58ecd3c7be07a915
chenzs <cezese@gmail.com>
2014-07-16 16:00:45 +0800

    .

--- src/okcoin_log.h
+++ src/okcoin_log.h
@@ -30,7 +30,7 @@
 //#include "mysql_conn_lib/include/mysql_driver.h"
 
 #include "mysql_connection.h"
-#include <cppconn/driver.h>
+#include "cppconn/driver.h"
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
